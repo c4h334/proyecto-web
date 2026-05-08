c97aa007890bb6d4099b4046f12759a74b2335b3
Jose Gabriel <gabochacon22@gmail.com>
2026-05-07 19:17:21 -0600

    Se agregó la sección del footer, que incluye los derechos reservados, el nombre del proyecto junto con sus redes sociales, además que se mantoene consistente con respecto al resto del sistema y también presente con el resto de la página

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/home.tsx";
 import Layout from "./components/layout/layout.tsx";
+import Contact from "./pages/contact.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact /></Layout>} />
         {/* Futuras rutas:
         <Route path="/store" element={<Layout><Store /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
         */}
       </Routes>
     </Router>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import Header from "./header.tsx";
-/*import Footer from "./footer.tsx"; */
+import Footer from "./footer.tsx";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,7 +11,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-grow">{children}</main>
-      {/* <Footer /> */}
+      { <Footer /> }
     </div>
   );
 };
