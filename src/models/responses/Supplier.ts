bed227c408e37ad2573ba6aa62c2aec3e3e950d4
Anderson Jesús Monge Alvarado <anderson.monge@ucr.ac.cr>
2026-05-26 16:22:51 -0600

    Revert merge accidental de frontend-backend

--- src/models/responses/Supplier.ts
+++
@@ -1,9 +0,0 @@
-export interface Supplier {
-  supplierResourceId: string;
-  companyName: string;
-  legalId: string;
-  location: string;
-  phone: string;
-  email: string;
-  productList: string;
-}
