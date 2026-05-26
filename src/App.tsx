a5767092c43ac9e65ce08d4b5f929d106966064d
Anderson Jesús Monge Alvarado <anderson.monge@ucr.ac.cr>
2026-05-26 16:22:51 -0600

    Revert merge accidental de frontend-backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { CustomerList } from "./components/CustomerList";
-import './App.css'
-import Home from "./pages/home.tsx";
-import Layout from "./components/layout/layout.tsx";
-import Contact from "./pages/contact.tsx";
-import Store from "./pages/store.tsx";
-        
-       
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
-        <Route path="/store" element={<Layout><Store /></Layout>} />
-        
-      </Routes>
-    </Router>
-  );
+  return <CustomerList />;
 }
 
 export default App;
