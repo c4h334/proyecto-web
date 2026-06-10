2d9e968434a069cfe8aa072582bca798870cae7a
c4h334 <anderson.monge@ucr.ac.cr>
2026-06-10 16:02:14 -0600

    Reapply "Merge pull request #17 from c4h334/develop"
    This reverts commit 63f312cd531120209618f0e2ebbce279c732d30e.

---
+++ src/models/responses/Supplier.ts
@@ -0,0 +1,9 @@
+export interface Supplier {
+  supplierResourceId: string;
+  companyName: string;
+  legalId: string;
+  location: string;
+  phone: string;
+  email: string;
+  productList: string;
+}
