63f312cd531120209618f0e2ebbce279c732d30e
c4h334 <anderson.monge@ucr.ac.cr>
2026-06-10 16:00:48 -0600

    Revert "Merge pull request #17 from c4h334/develop"
    This reverts commit 50f1befc6b5436b8cb07ab05bf21ecbd1330925e, reversing
    changes made to bed227c408e37ad2573ba6aa62c2aec3e3e950d4.

--- src/services/contenful.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'contentful';
-
-export const client = createClient({
-  // Vite utiliza import.meta.env para acceder a las variables del .env
-  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
-});
